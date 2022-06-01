24554a266b1bec2ac513f8efe260bd010f36f8c2
Michael Washburn <96266335+mwashburn1987@users.noreply.github.com>
2022-06-01 09:47:35 -0700

    Time: 101 ms (16.63%), Space: 42 MB (89.41%) - LeetHub

---
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    
+    //use recursion
+    //check for node value, if exists swap children values;
+
+    
+    if(root) {
+        swap(root);
+        invertTree(root.left);
+        invertTree(root.right);
+    }
+    
+    //return tree
+    return root;
+    
+};
+
+    //swap function
+const swap = (node) => {
+    let temp = node.left;
+    node.left = node.right
+    node.right = temp;
+}
