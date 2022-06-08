2c28c28b941baad965f3c3274b8934fd5c00c5da
Michael Washburn <96266335+mwashburn1987@users.noreply.github.com>
2022-06-08 13:16:24 -0700

    Time: 190 ms (9.47%), Space: 44.5 MB (74.15%) - LeetHub

--- 383-ransom-note/383-ransom-note.js
+++ 383-ransom-note/383-ransom-note.js
@@ -4,28 +4,19 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    
-    //declare map for storage
-    const map = new Map()
-    
-    //loop through magazine
-    for (i = 0; i < magazine.length; i ++) {
-        
-        if(map.has(magazine[i])) {
-            map.set(magazine[i], map.get(magazine[i]) + 1)
-        } else {
-            map.set(magazine[i], 1)
-        }
+    let ob = {};
+    for(let i = 0; i < magazine.length; i++) {
+        if(ob[magazine[i]]) {
+            ob[magazine[i]] ++;
+        } else ob[magazine[i]] = 1;
     }
     
-    //loop through note
-    for (i = 0; i < ransomNote.length; i++) {
-        if (!map.has(ransomNote[i]) || map.get(ransomNote[i]) === 0) {
+    for(let j = 0; j < ransomNote.length; j++) {
+        if(!ob[ransomNote[j]] || ob[ransomNote[j]] <= 0) {
             return false;
         } else {
-            map.set(ransomNote[i], map.get(ransomNote[i]) - 1)
+            ob[ransomNote[j]] --;
         }
     }
-    //return
     return true;
 };
