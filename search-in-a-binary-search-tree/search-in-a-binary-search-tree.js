95ab28bc6660a5a73b4dced59c8252298b52516d
Michael Washburn <96266335+mwashburn1987@users.noreply.github.com>
2022-07-04 16:58:59 -0700

    Time: 116 ms, Memory: 49.5 MB - LeetHub

--- search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
+++ search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -11,16 +11,34 @@
  * @param {number} val
  * @return {TreeNode}
  */
+
+//recursive
 // var searchBST = function(root, val) {
 //     if(!root) return null;
 //     if(root.val === val) return root; 
 //     if(val < root.val) return searchBST(root.left, val);
-//     if(val > root.val) return searchBST(root.right. val);
+//     if(val > root.val) return searchBST(root.right, val);
 // };
 
+//iterative
 var searchBST = function(root, val) {
+    let stack = [];
+    
     if(!root) return null;
     if(root.val === val) return root;
-    if(val < root.val) return searchBST(root.left, val);
-    if(val > root.val) return searchBST(root.right, val);
+    
+    stack.push(root);
+    
+    while(stack.length) {
+        const node = stack.pop();
+        if(!node) continue;
+        if (node.val === val) return node;
+        
+        if(val < node.val) {
+            stack.push(node.left)
+        } else {
+            stack.push(node.right);
+        }
+    }
+    return null;
 }
